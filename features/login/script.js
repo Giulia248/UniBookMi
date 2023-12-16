c8f12a8776cb12a9381021e28f8242fe49076d7b
Paolo (fake) <>
2023-12-16 10:25:44 +0100

    [006] FIX login

--- features/login/script.js
+++ features/login/script.js
@@ -2,18 +2,12 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
-
-
-
-window.addEventListener("DOMContentLoaded", (event) => {
     registerBtn.addEventListener('click', () => {
     
         container.classList.add("active");
     });
-});
 
-window.addEventListener("DOMContentLoaded", (event) => {
+
     loginBtn.addEventListener('click', () => {
         container.classList.remove("active");
     });
-});
