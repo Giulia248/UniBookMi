3d9ebe32242290c8079f7e6fbb45fe1064cc35f2
Giulia Floris <>
2024-01-25 12:54:33 +0100

    [014] FIX links

--- features/home/mainScript.js
+++ features/home/mainScript.js
@@ -8,6 +8,6 @@ document.getElementById("goToUserInfo").addEventListener("click", function (even
 // goto news
 document.getElementById("goToNews").addEventListener("click", function (event) {
     event.preventDefault(); // Prevent the default form submission
-    window.location.href = 'http://127.0.0.1:5500/features/userInfo/UserInfoView.html';
+    window.location.href = 'http://127.0.0.1:5500/features/news/NewsView.html';
 
 });
