ecd568d7a2a43ed857dab27b1e76aa14b4a36743
Giulia Floris <>
2024-01-25 11:26:46 +0100

    [014] ADD home page template

--- features/login/script.js
+++ features/login/script.js
@@ -14,7 +14,7 @@ const loginBtn = document.getElementById('login');
 
     document.getElementById("signIn").addEventListener("click", function(event) {
         event.preventDefault(); // Prevent the default form submission
-        window.location.href= 'http://127.0.0.1:5500/features/main.html';
+        window.location.href= 'http://127.0.0.1:5500/features/home/index.html';
 
       });
 
