d65cbc3736c230261a9226bb3a01066c6fa168d4
Paolo <>
2024-02-07 12:13:02 +0100

    [011] ADD files

--- core/database/config/database.js
+++ core/database/config/database.js
@@ -1,11 +1,34 @@
-const { createPool } = require("mysql");
+var mysql = require('mysql');
 
-const pool = createPool({ port: 3306,
-    host: "localhost",
+var con = mysql.createConnection({
+    host: "127.0.0.1",
     user: "root",
-    password: "",
-    database: "test",
-    connectionLimit: 10
-    });
+    password: "Paolo2002-"
+  });
 
-    module.exports = pool;
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("DB connected !");
+});
+
+            
+
+/*
+
+// Creazione del server web
+const path = require('path');
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const port = 3306;
+
+
+// Connessione al database
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "Paolo2002-",
+  database: "UniBookMi"
+});
+
+*/
